f1e64191fef841a87305fc902d909c525e0e5bc0
timtinan37 <timtinan@outlook.com>
2018-12-29 23:00:06 +0600

    commit in ubuntu

--- productHunt/settings.py
+++ productHunt/settings.py
@@ -76,17 +76,30 @@ WSGI_APPLICATION = 'productHunt.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'producthuntdb',
         'USER': 'postgres',
         'PASSWORD': 'TarannumPostgres',
-        'HOST': 'localhost',
+        'HOST': 'productHunt',
         'PORT': '5432',
     }
 }
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+
 
 
 # Password validation
--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import textwrap
 # Create your models here.
+class Product(models.Model):
+	title = models.CharField(max_length=200)
+	url = models.TextField()
+	pub_date = models.DateTimeField(auto_now_add=True)
+	votes_total = models.BigIntegerField(default=0)
+	image = models.ImageField(upload_to='images/%Y/%m/%d/')
+	icon = models.ImageField(upload_to='icons/%Y/%m/%d/')
+	body = models.TextField()
+	hunter = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+	def pub_date_pretty(self):
+		return self.pub_date.strftime("%d %B %Y")
+
+	def summary(self):
+		return textwrap.shorten(self.body, width=100, placeholder="...")
+
+	def __str__(self):
+		return self.title
