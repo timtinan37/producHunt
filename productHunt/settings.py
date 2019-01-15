a0797314b7002500915c50f54b5cec636998f0a6
Tariq Imtinan <timtinan@outlook.com>
2019-01-15 19:40:25 +0600

    Update settings.py

--- productHunt/settings.py
+++ productHunt/settings.py
@@ -76,19 +76,7 @@ WSGI_APPLICATION = 'productHunt.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'producthuntdb',
-        'USER': 'postgres',
-        'PASSWORD': 'TarannumPostgres',
-        'HOST': 'productHunt',
-        'PORT': '5432',
-    }
-}
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
